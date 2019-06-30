a082c7f964dbd8bfa57fa8a7510cad1de8080216
Anatoliy <izze.pochta@gmail.com>
2019-06-30 21:36:49 +0300

    cards

--- src/js/components/MainCards.vue
+++ src/js/components/MainCards.vue
@@ -21,27 +21,39 @@
                 <div class="dancer-profile-image" style="background-image: url('https://pp.userapi.com/c831108/v831108502/155963/-Gx7aOfdWY8.jpg')"></div>
             </div>
         </div>
-        <div class="dancer-profile-container">
+        <div class="dancer-profile-container"
+             @mouseenter="enter"
+             @mousemove="move"
+             @mouseleave="leave">
             <div class="dancer-profile-avatar">
                 <div class="dancer-profile-discription">Имя Фамилия</div>
                 <div class="dancer-profile-image" style="background-image: url('https://pp.userapi.com/c831108/v831108502/155959/UdtFgd1DQ34.jpg')"></div>
             </div>
         </div>
 
-        <div class="dancer-profile-container">
+        <div class="dancer-profile-container"
+             @mouseenter="enter"
+             @mousemove="move"
+             @mouseleave="leave">
             <div class="dancer-profile-avatar">
                 <div class="dancer-profile-discription">Имя Фамилия</div>
                 <div class="dancer-profile-image" style="background-image: url('https://pp.userapi.com/c831108/v831108502/155963/-Gx7aOfdWY8.jpg')"></div>
             </div>
         </div>
-        <div class="dancer-profile-container">
+        <div class="dancer-profile-container"
+             @mouseenter="enter"
+             @mousemove="move"
+             @mouseleave="leave">
             <div class="dancer-profile-avatar">
                 <div class="dancer-profile-discription">Имя Фамилия</div>
                 <div class="dancer-profile-image" style="background-image: url('https://pp.userapi.com/c831108/v831108502/155959/UdtFgd1DQ34.jpg')"></div>
             </div>
         </div>
 
-        <div class="dancer-profile-container">
+        <div class="dancer-profile-container"
+             @mouseenter="enter"
+             @mousemove="move"
+             @mouseleave="leave">
             <div class="dancer-profile-avatar">
                 <div class="dancer-profile-discription">Имя Фамилия</div>
                 <div class="dancer-profile-image" style="background-image: url('https://pp.userapi.com/c831108/v831108502/155963/-Gx7aOfdWY8.jpg')"></div>
@@ -53,44 +65,41 @@
 <script>
     export default {
         name: "MainCards",
+        props: ['coefficient'],
         data(){
             return {
-                coeff: 15,
-                elem: null
+                container: null,
+                avatar: null
             }
         },
         methods: {
             enter(e)  {
-                console.log('enter');
-                console.log(e.target);
-                this.elem = e.target
-                let coords = this.calculate(this.elem, e);
-                this.elem.style.zIndex = 1
-                let avatar = this.elem.getElementsByClassName('dancer-profile-avatar')[0]
-                avatar.style.trasition = "all .3s ease"
-                avatar.style.transform = "perspective(450px)  rotateX(" + coords.x + "deg) rotateY(" + coords.y + "deg) scale3d(1.05, 1.05, 1.05)"
+                this.container = e.target
+                this.avatar = this.container.getElementsByClassName('dancer-profile-avatar')[0]
+                let coords = this.calculate(e)
+                this.container.style.zIndex = 1
+                this.avatar.style.trasition = "all .3s ease"
+                this.avatar.style.transform = "perspective(450px)  rotateX(" + coords.x + "deg) rotateY(" + coords.y + "deg) scale3d(1.05, 1.05, 1.05)"
             },
             move(e) {
-                console.log(e.target);
-                let coords = this.calculate(this.elem, e);
-                this.elem.style.zIndex = 1
-                let avatar = this.elem.getElementsByClassName('dancer-profile-avatar')[0]
-                avatar.style.trasition = "none"
-                console.log(coords);
-                avatar.style.transform = "perspective(450px)  rotateX(" + coords.x + "deg) rotateY(" + coords.y + "deg) scale3d(1.05, 1.05, 1.05)"
+                let coords = this.calculate(e);
+                this.container.style.zIndex = 1
+                this.avatar.style.trasition = "none"
+                this.avatar.style.transform = "perspective(450px)  rotateX(" + coords.x + "deg) rotateY(" + coords.y + "deg) scale3d(1.05, 1.05, 1.05)"
             },
-            leave(e) {
-                console.log(e.target);
+            leave() {
+                this.container.style.zIndex = "auto"
+                this.avatar.style.transition = "all .3s ease"
+                this.avatar.style.transform = "perspective(450px)  rotateX(" + 0 + "deg) rotateY(" + 0 + "deg) scale3d(1, 1, 1)"
             },
-            calculate(elem, event){
-                let XRel = event.pageX - elem.offsetLeft;
-                let YRel = event.pageY - elem.offsetTop;
-                let width = elem.offsetWidth;
-                let height = elem.offsetHeight;
-                console.log(elem.offsetLeft);
-                console.log(elem.offsetTop);
-                let XAngle = -(0.5 - (YRel / height)) * this.coeff;
-                let YAngle = (0.5 - (XRel / width)) * (this.coeff / 2);
+            calculate(event){
+                let rect = this.container.getBoundingClientRect();
+                let XRel = event.pageX - (rect.left + window.scrollX);
+                let YRel = event.pageY - (rect.top + window.scrollY);
+                let width = this.container.offsetWidth;
+                let height = this.container.offsetHeight;
+                let XAngle = -(0.5 - (YRel / height)) * this.coefficient;
+                let YAngle = (0.5 - (XRel / width)) * (this.coefficient / 2);
 
                 return {
                     x: XAngle,
